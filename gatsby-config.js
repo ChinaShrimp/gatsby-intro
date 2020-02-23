ad00d69bd6a7229ded1470ff2ee07686f4482b24
Lyon Liang <lyon.liang12@gmail.com>
2020-02-23 23:30:49 +0800

    text blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,16 @@ module.exports = {
       resolve: "gatsby-plugin-mdx",
       options: {
         defaultLayouts: {
-          default: require.resolve("./src/components/layout.js")
-        }
-      }
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "posts",
+      },
     },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import usePosts from "../hooks/use-posts";
+import PostPreview from "../components/post-preview";
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <div> Hi Frontend Masters! </div>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Read My Blog</h1>
+      <ul>
+        {posts.map(post => {
+          return <PostPreview key={post.slug} post={post} />;
+        })}
+      </ul>
+    </Layout>
+  );
+};
