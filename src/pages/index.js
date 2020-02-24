f4c354cf7b0d8246a09cc0d3844053d023a3b3d4
Lyon Liang <lyon.liang12@gmail.com>
2020-02-24 14:23:47 +0800

    mdx images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,22 @@ import React from "react";
 import Layout from "../components/layout";
 import usePosts from "../hooks/use-posts";
 import PostPreview from "../components/post-preview";
+import Hero from "../components/hero";
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Read My Blog</h1>
-      <ul>
-        {posts.map(post => {
-          return <PostPreview key={post.slug} post={post} />;
-        })}
-      </ul>
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h1>Read My Blog</h1>
+        <ul>
+          {posts.map(post => {
+            return <PostPreview key={post.slug} post={post} />;
+          })}
+        </ul>
+      </Layout>
+    </>
   );
 };
